36999e3e132da61b7590b27221f0e78c9d245f06
Anton Vectura <vectura1986@gmail.com>
2017-03-28 03:24:52 +0300

    28.03.2017 3:24

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,10 @@
 $(function(){
+
+    $(".toggle-mnu").click(function() {
+      $(this).toggleClass("on");
+      $(".main-mnu").slideToggle();
+      return false;
+  });
 	
 	$(".owl-carousel").owlCarousel({
 		loop:true,
